35dfe462aef7c54abb04ae6651e6c30111c96197
Kausik D <89984370+Kaus1kC0des@users.noreply.github.com>
2022-08-02 22:09:15 +0530

    Add files via upload

---
+++ Book_Store_App/Using Databases/app.py
@@ -0,0 +1,49 @@
+from utils import databaseops
+USER_CHOICE = """
+Enter:
+- 'a' to add a new book
+- 'l' to list all books
+- 'r' to mark a book as read
+- 'd' to delete a book
+- 'q' to quit
+
+Your choice: """
+
+
+def menu():
+    databaseops.create_table()
+    user_input = input(USER_CHOICE)
+    while user_input != 'q':
+        if user_input == 'a':
+            prompt_add_book()
+        elif user_input == 'l':
+            list_books()
+        elif user_input == 'r':
+            prompt_read_book()
+        elif user_input == 'd':
+            prompt_delete_book()
+
+        user_input = input(USER_CHOICE)
+
+
+def prompt_add_book():
+    name = input('Enter the new book name: ')
+    author = input('Enter the new book author: ')
+    databaseops.add_book(name,author)
+
+def list_books():
+    databaseops.list_books()
+
+def prompt_read_book():
+    name = input('Enter the name of the book you just finished reading: ')
+    databaseops.mark_as_read(name)
+
+
+
+def prompt_delete_book():
+    name = input('Enter the name of the book you wish to delete: ')
+
+    databaseops.delete_book(name)
+
+
+menu()

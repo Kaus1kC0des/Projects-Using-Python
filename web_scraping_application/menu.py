4f53de05b32320e1638fa0d57dd61e23aa33a14e
Kausik D <89984370+Kaus1kC0des@users.noreply.github.com>
2022-12-26 21:27:08 +0530

    Add files via upload

---
+++ web_scraping_application/menu.py
@@ -0,0 +1,40 @@
+from app import books
+
+
+USER_CHOICES = """Select any one of the following:
+- 'b' to look at 5-star books
+- 'c' to look at cheapest books
+- 'n' to just get the next available book on the page
+- 'q' to exit the application
+Your Choice: """
+
+def menu():
+    USER_INPUT = input(USER_CHOICES)
+    while USER_INPUT.title() != 'Q':
+        if USER_INPUT.title() == 'B':
+            best_books()
+        elif USER_INPUT.title() == 'C':
+            cheapest_books()
+        elif USER_INPUT.title() == 'N':
+            next_book()
+        else:
+            print("Kindly enter a valid input.")
+        USER_INPUT = input(USER_CHOICES)
+
+
+def best_books():
+    bbooks = sorted(books,key=lambda x : x.rating * -1)[:10]
+    for boosk in bbooks:
+        print(boosk)
+
+def cheapest_books():
+    cbooks = sorted(books,key=lambda x:x.price)[:10]
+    for cbook in cbooks:
+        print(cbook)
+
+book_generator = (x for x in books)
+def next_book():
+    print(next(book_generator))
+
+
+menu()
